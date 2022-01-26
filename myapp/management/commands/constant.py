00082d887770cbde0d1568602b64e0142a747a2e
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2022-01-26 11:15:05 +0500

    sdf

---
+++ myapp/management/commands/constant.py
@@ -0,0 +1,26 @@
+from utils import *
+done = "✅"
+prev = "⏮"
+next = "⏭"
+comment = "💬"
+cross = "❌"
+trash = "🗑"
+reload = "🔄"
+
+NAME, DESCRIPTION, NUMBER, DESCRIPTION, WAIT, MENU, SELECT_REQUEST_TYPE, GET_TEMPLATE, SELECT_CONFIRMERS, CHECK_REQUEST_TRUE_OR_FALSE, GET_COMMENT_FOR_REQUEST = range(11)
+
+TOKEN = "2136261295:AAGRm85AIjpG6hspmOwNtNUgPX_F6EJ7pUA"
+
+
+
+
+request_statuses = [
+    "kutilmoqda", "qabul qilindi", "bekor qilindi"
+]
+
+checkkkk = [
+    'Fikringiz qabul qilindi va so\'rov tasdiqlandi!',
+    "Fikringiz qabul qilindi va so'rov bekor qilindi!"
+]
+
+host = "http://167.172.128.11"
