9d220fe8f9e9114962b12fa772befa777a8aca76
MHojimurod <cool.hojimurod2001@mail.ru>
2022-01-27 11:59:03 +0500

    dsf

--- myapp/management/commands/main.py
+++ myapp/management/commands/main.py
@@ -2,26 +2,45 @@ from telegram.ext import *
 from telegram import *
 from myapp.management.commands.constant import *
 from myapp.management.commands.decorators import *
-class Bot(Updater):
+from myapp.management.commands.register import Register
+class Bot(Updater, Register):
     def __init__(self, token: str = None):
         assert token, ValueError("Token is required")
         super().__init__(token)
 
+        not_start = ~Filters.regex("^(\/start)")
+
         self.conversation = ConversationHandler(
             entry_points=[
-                CommandHandler('start',self.start_handler)
+                CommandHandler('start',self.start)
                 ],
-            states={},
-            fallbacks=[]
+            states={
+                LANGUAGE: [MessageHandler(Filters.regex("^(🇺🇿|🇷🇺)") & not_start, self.language)],
+                NAME: [MessageHandler(Filters.text & not_start, self.name) ],
+                NUMBER: [MessageHandler(Filters.contact & not_start, self.number) ],
+                REGION: [MessageHandler(Filters.text & not_start, self.region) ],
+                DISTRICT: [MessageHandler(Filters.text & not_start, self.district) ],
+                SHOP: [MessageHandler(Filters.text & not_start, self.shop) ],
+                MENU: [
+                    # MessageHandler(Filters.regex("^(Sotib olish|Купить)"), self.buy),
+                    # MessageHandler(Filters.regex("^(Sotib olingan|Купленные)"), self.purchased),
+                    # MessageHandler(Filters.regex("^(Mening ballarim|Мои баллы)"), self.my_balls)
+                    ],
+                # SELECT_CATEGORY: [MessageHandler(Filters.regex("^\d+$") & not_start, self.buy), CallbackQueryHandler(self.buy, pattern="^category_pagination"), CallbackQueryHandler(self.start, pattern="^cancel_pagination")],
+            },
+            fallbacks=[
+                CommandHandler('start',self.start)
+            ]
         )
         self.dispatcher.add_handler(self.conversation)
+
         self.start_polling()
         print('polling')
         self.idle()
+    
 
+    
+    
+            
         
-    @register
-    def start_handler(self, update:Update, context:CallbackContext):
-        user, db_user = get_user(update)
-        print(user,db_user)
-bot = Bot(TOKEN)
+x = Bot(TOKEN)
--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-
+from admin_panel.models import Regions,District,Text
 # Create your models here.
-class Seller(models.Model):
-    chat_id = models.IntegerField()
-    language = models.IntegerField(choices=((0, 'uz'), (1, 'ru')))
 
     
+class Seller(models.Model):
+    chat_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=100)
+    region = models.ForeignKey(Regions, on_delete=models.CASCADE, null=True)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, null=True)
+    shop = models.CharField(max_length=300,null=True,blank=True)
+    language = models.IntegerField(choices=((0, 'uz'), (1, 'ru')))
+    
+    def text(self, name):
+        text = Text.objects.filter(name=name)
+        return (text.first().uz_data if self.language == 0 else text.first().ru_data) if text.exists() else ""
