621bbfe202ff99143f706aa6680abdafab5a1bb5
MHojimurod <cool.hojimurod2001@mail.ru>
2022-02-04 15:08:40 +0500

    xxx

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'cvitations/'
 MEDIA_ROOT = BASE_DIR / 'cvitations'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
