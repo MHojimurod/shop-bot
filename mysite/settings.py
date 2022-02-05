6a3d0a49d0a93809fcaae06cafd19248244c00b5
MHojimurod <cool.hojimurod2001@mail.ru>
2022-02-05 10:42:58 +0500

    gf

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,8 +131,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = 'cvitations/'
-MEDIA_ROOT = BASE_DIR / 'cvitations'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
