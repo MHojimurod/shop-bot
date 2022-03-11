977d2b3f638599a5be7ede4fbb255612a584e028
MHojimurod <cool.hojimurod2001@mail.ru>
2022-03-11 15:54:44 +0500

    bat file push

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,11 +78,30 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "bots",
+
+        'USER': 'db_user',
+
+        'PASSWORD': 'user_id',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
+
 }
 
 
