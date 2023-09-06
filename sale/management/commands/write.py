22760516f00b2d634df14867cb3eff9b01ec7b37
MHojimurod <cool.hojimurod2001@mail.ru>
2023-09-06 18:15:14 +0500

    update

--- sale/management/commands/write.py
+++ sale/management/commands/write.py
@@ -1,36 +1,24 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
-from sale.models import SerialNumbers
+from sale.models import SaleSeller
+import requests
+import time
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        excel = open('sale/management/commands/excel.xlsx', 'rb')
-        for i in range(4):
-            df = pd.read_excel(excel, sheet_name=i).to_dict()
-            code = df.get("code", None)
-            price = df.get("price", None)
-            SerialNumbers
-            for key, value in code.items():
-                SerialNumbers.objects.get_or_create(code=value, cashback=price[key])
-                
+        sellers = SaleSeller.objects.all()
+        count = 0
+        for i in sellers:
+            text = "Bot yangilandi, qayta ishga tushirish uchun /start buyrug'ini yuboring"
+            try:
         
-        _list = [
-            {
-                "code":1217581,
-                "cashback":5
-            },
-            {
-                "code":1229247,
-                "cashback":7
-            },
-            {
-                "code":1835610,
-                "cashback":10
-            },
-            {
-                "code": 1440870,
-                "cashback":100
-            }
-        ]
-        for i in _list:
-            SerialNumbers.objects.get_or_create(**i)
+                res = requests.get(f"https://api.telegram.org/bot6525921476:AAHn9ocU5-ik7TMuFScvpAw6BAlJwrpywkI/sendMessage?text={text}&chat_id={i.chat_id}")
+                print(res.json()['ok'])
+
+            except:
+                print("not send message")
+            
+            count+=1
+            if count == 20:
+                time.sleep(1)
+        print("end")
